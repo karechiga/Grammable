a4ddaccdc618263f4027b97f89c5d1f431c2c6dd
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-06-22 22:23:56 +0000

    added picture validation

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,8 @@
 class Gram < ApplicationRecord
   mount_uploader :picture, PictureUploader
+  
   validates :message, presence: true, length: { minimum: 3 }
+  validates :picture, presence: true
 
   belongs_to :user
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  include ActionDispatch::TestProcess
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
