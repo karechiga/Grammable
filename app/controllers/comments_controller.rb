ddcc8a3e7bc0cad9f2682535c7406d04c6e3eb91
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-06-23 21:35:05 +0000

    tested comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+    @gram = Gram.find_by_id(params[:gram_id])
+    if !@gram.blank?
+      @comment = @gram.comments.create(comment_params)
+      redirect_to root_path
+    else
+      render_not_found(:not_found)
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
+end
